e9fa4da5a1d641b7de76306be978b91651342cf2
Salma-Raf <rksalma6@gmail.com>
2023-11-22 16:44:45 +0100

    2

--- src/main/java/com/example/Entities/Concours.java
+++ src/main/java/com/example/Entities/Concours.java
@@ -1,5 +1,39 @@
 package com.example.Entities;
 
-public class Concours {
+import java.io.Serializable;
+import java.util.Date;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Concours implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Integer Id;
+	private String libelle;
+	private Date jourPassage;
+	private String nomSalle;
+	private Integer duree;
+	private String etat;
+	private Integer maxAge;
+	private Integer minNote;
+	private Boolean hasBacLibre;
+	private String Categorie;
 
 }
